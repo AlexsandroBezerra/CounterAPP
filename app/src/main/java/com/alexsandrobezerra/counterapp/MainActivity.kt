2c768efac675f151a11395f9c57d0eb675c6a54e
Sandro Bezerra <alexsandro.g.bezerra@gmail.com>
2021-10-16 10:39:51 -0300

    refactor: counter private property

--- app/src/main/java/com/alexsandrobezerra/counterapp/MainActivity.kt
+++ app/src/main/java/com/alexsandrobezerra/counterapp/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    val counter = Counter()
+    private val counter = Counter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
